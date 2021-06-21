5b522ada7ef39882bbbb0835e26ea8d526a01066
Kasper Chen <neko12377@users.noreply.github.com>
2021-06-22 04:04:05 +0800

    feat: copy button

---
+++ src/components/molecule/CopyButton/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {Button} from "module/home/component/atom/Button";
+
+interface CopyButtonProps {
+    children?: React.ReactElement | string;
+    targetRef: React.MutableRefObject<any>;
+    buttonRef: React.MutableRefObject<HTMLButtonElement>;
+}
+
+export const CopyButton = (props: CopyButtonProps) => {
+    const handleClick = (targetRef: React.MutableRefObject<any>) => {
+        const text = targetRef.current.innerText ?? "";
+        navigator.clipboard.writeText(text).then();
+    };
+    const handleHover = (
+        buttonRef: React.MutableRefObject<HTMLButtonElement>
+    ) => {
+        buttonRef.current.style.backgroundColor = "#52c8f9";
+        buttonRef.current.style.border = "1px solid #35bff8";
+        buttonRef.current.style.backgroundImage = "none";
+        buttonRef.current.style.color = "#ffffff";
+    };
+    const handleLeave = (
+        buttonRef: React.MutableRefObject<HTMLButtonElement>
+    ) => {
+        buttonRef.current.style.backgroundColor = "none";
+        buttonRef.current.style.border = "solid 1px #b6b6b6";
+        buttonRef.current.style.backgroundImage =
+            "linear-gradient(to top, #f7f7f7, #ffffff)";
+        buttonRef.current.style.color = "#5e5e5e";
+    };
+    const handlePress = (
+        buttonRef: React.MutableRefObject<HTMLButtonElement>
+    ) => {
+        buttonRef.current.style.backgroundColor = "#36a9fb";
+        buttonRef.current.style.border = "solid 1px #35bff8";
+        buttonRef.current.style.backgroundImage = "none";
+        buttonRef.current.style.color = "#ffffff";
+    };
+    const handleUp = (buttonRef: React.MutableRefObject<HTMLButtonElement>) => {
+        buttonRef.current.style.backgroundColor = "#52c8f9";
+        buttonRef.current.style.border = "1px solid #35bff8";
+        buttonRef.current.style.backgroundImage = "none";
+        buttonRef.current.style.color = "#ffffff";
+    };
+    return (
+        <Button
+            backgroundImage="linear-gradient(to top, #f7f7f7, #ffffff)"
+            backgroundColor="none"
+            border="solid 1px #b6b6b6"
+            color="#5e5e5e"
+            handleClick={() => handleClick(props.targetRef)}
+            ref={props.buttonRef}
+            handleHover={() => handleHover(props.buttonRef)}
+            handleLeave={() => handleLeave(props.buttonRef)}
+            handlePress={() => handlePress(props.buttonRef)}
+            handleUp={() => handleUp(props.buttonRef)}
+        >
+            {props.children}
+        </Button>
+    );
+};
