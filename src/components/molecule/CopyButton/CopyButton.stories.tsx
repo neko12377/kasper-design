5e9d24e9e1ef078f14e3fd783bbbe33e09c3ec0a
Kasper Chen <neko12377@gmail.com>
2021-07-31 21:11:28 +0800

    feat: add copy button stories

--- src/components/molecule/CopyButton/CopyButton.stories.tsx
+++ src/components/molecule/CopyButton/CopyButton.stories.tsx
@@ -1,6 +1,28 @@
-import {CopyButton} from "./index";
+import {CopyButton, CopyButtonProps} from "./index";
+import {Story} from "@storybook/react";
+import React, {useRef} from "react";
+import styled from "styled-components";
+
+const Target = styled.div`
+    width: auto;
+    height: 20px;
+    margin-right: 10px;
+`;
 
 export default {
     title: "Components/CopyButton",
     component: CopyButton,
+}
+
+export const Basic: Story<CopyButtonProps> = () => {
+    const buttonRef = useRef(document.createElement("button"));
+    const targetRef = useRef(document.createElement("div"));
+    return (
+        <React.Fragment>
+            <Target ref={targetRef}>Copy me!</Target>
+            <CopyButton targetRef={targetRef} buttonRef={buttonRef}>
+                Copy
+            </CopyButton>
+        </React.Fragment>
+    )
 }
